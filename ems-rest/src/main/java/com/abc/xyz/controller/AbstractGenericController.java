2f70a7e69f765e12c6860dae2da3ac274959f9c7
veer <veer@daniel>
2017-06-10 13:18:12 +0530

    controller initialize

---
+++ ems-rest/src/main/java/com/abc/xyz/controller/AbstractGenericController.java
@@ -0,0 +1,5 @@
+package com.abc.xyz.controller;
+
+public abstract class AbstractGenericController {
+	
+}
---
+++ ems-rest/src/main/java/com/abc/xyz/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.abc.xyz.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.abc.xyz.model.EmployeeModel;
+import com.abc.xyz.response.ResponseEntity;
+
+@RestController
+public class EmployeeController extends AbstractGenericController{
+
+	@RequestMapping(value="/employee", method=RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<?> create(@RequestBody EmployeeModel employeeModel) {
+		final ResponseEntity<?> responseEntity = new ResponseEntity<>();
+		
+		try {
+			//create employee business call
+			responseEntity.setSuccess(true);
+			responseEntity.setCode(200);
+			responseEntity.setMessage("Successfully created employee");
+		} catch (Exception e) {
+			responseEntity.setSuccess(false);
+			responseEntity.setCode(500);
+			responseEntity.setMessage(e.getMessage());
+		}
+		
+		return responseEntity;
+	}
+}
